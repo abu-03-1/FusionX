60d7b2c9a15f5cd763a2179905f1c6b1e8c6b693
Ummu024 <ummukulsu207@gmail.com>
2026-08-22 11:51:55 +0530

    Configure application routing and Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD3n0pAc0BlKCB2P_Ycy09-zSQh6SZjAu0",
+  authDomain: "dayflow-hrms-41604.firebaseapp.com",
+  projectId: "dayflow-hrms-41604",
+  storageBucket: "dayflow-hrms-41604.firebasestorage.app",
+  messagingSenderId: "713838661904",
+  appId: "1:713838661904:web:e6e2e47fcd7db266b71595",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
